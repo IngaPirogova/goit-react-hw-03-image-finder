6fa4109622e04ad3dc7f6d58995c3633ccc5f3b8
IngaPirogova <ingeborga.pirogova@gmail.com>
2023-01-17 22:16:10 +0200

    Upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,25 @@ import { Searchbar } from './Searchbar/Searchbar';
 export class App extends React.Component {
   state = {
     searchName: '',
+    pictures: null,
   };
 
   handleFormSubmit = searchName => {
     this.setState({ searchName });
   };
+
+componentDidUpdate(prevProps, prevState) {
+  if (prevProps.searchName !== this.props.searchName) {
+    console.log('prevprops.searchName: ', prevProps.searchName)
+    console.log('this.props.searchName: ', this.props.searchName)
+
+    fetch('https://pixabay.com/api/?q=cat&page=1&key=31233349-657dbeb08b09bae80b555b3c4&image_type=photo&orientation=horizontal&per_page=12')
+    .then(res => res.json())
+    .then(console.log)
+  }
+}
+
+
 
   render() {
     return (
